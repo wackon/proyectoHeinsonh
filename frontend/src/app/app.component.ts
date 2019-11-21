3a8f407baa42d15bae952429e34adceb2744a1b8
camilo <camilomontoya2@gmail.com>
2019-11-21 15:21:12 -0500

    componentes de navegacion

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from "@angular/core";
+import { NavbarComponent } from "angular-bootstrap-md";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.sass"]
 })
 export class AppComponent {
-  title = 'frontend';
+  @ViewChild("navbar", { static: true }) navbar: NavbarComponent;
+  title = "frontend";
 }
